b3fe1d807d3d5daea4b2e851c9f914663666c15a
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-08-21 17:58:20 +0300

    Fix UI layout issue and add new test element in AmplitudePlayer.js.

--- app/AmplitudePlayer.js
+++ app/AmplitudePlayer.js
@@ -34,71 +34,75 @@ const AmplitudePlayer = () => {
           className="album-art"
         />
         <div className="amplitude-visualization" id="large-visualization"></div>
-        <div id="player-left-bottom">
+        <div className="testplayer">
         <div>
         <Image src={"/image.svg"} width={154} height={129} alt="album art"/>
         </div>
+        <div id="player-left-bottom">
+        
           
-         <div className="iconsstyle">
-           <div id="control-container">
-            <div id="repeat-container">
-              <div className="amplitude-repeat" id="repeat"></div>
-              <div
-                className="amplitude-shuffle amplitude-shuffle-off"
-                id="shuffle"
-              ></div>
-            </div>
-            <div id="central-control-container">
-              <div id="central-controls">
-                <div className="amplitude-prev" id="previous"></div>
-                <div className="amplitude-play-pause" id="play-pause"></div>
-                <div className="amplitude-next" id="next"></div>
-              </div>
-            </div>
-            <div id="volume-container">
-              <div className="volume-controls">
-                <div className="amplitude-mute amplitude-not-muted"></div>
-                <input type="range" className="amplitude-volume-slider" />
-                <div className="ms-range-fix"></div>
-              </div>
-              <div
-                className="amplitude-shuffle amplitude-shuffle-off"
-                id="shuffle-right"
-              ></div>
-            </div>
-          </div>
-         <div id="time-container">
-            {/* <span className="current-time">
-              <span className="amplitude-current-minutes"></span>:
-              <span className="amplitude-current-seconds"></span>
-            </span> */}
-            <div id="progress-container">
-              <div className="amplitude-wave-form"></div>
-              <input type="range" className="amplitude-song-slider" />
-              <progress
-                id="song-played-progress"
-                className="amplitude-song-played-progress"
-              ></progress>
-              <progress
-                id="song-buffered-progress"
-                className="amplitude-buffered-progress"
-                value="0"
-              ></progress>
-            </div>
-            {/* <span className="duration">
-              <span className="amplitude-duration-minutes"></span>:
-              <span className="amplitude-duration-seconds"></span>
-            </span> */}
-          </div> 
+        <div className="iconsstyle">
+          <div id="control-container">
+           <div id="repeat-container">
+             <div className="amplitude-repeat" id="repeat"></div>
+             <div
+               className="amplitude-shuffle amplitude-shuffle-off"
+               id="shuffle"
+             ></div>
+           </div>
+           <div id="central-control-container">
+             <div id="central-controls">
+               <div className="amplitude-prev" id="previous"></div>
+               <div className="amplitude-play-pause" id="play-pause"></div>
+               <div className="amplitude-next" id="next"></div>
+             </div>
+           </div>
+           <div id="volume-container">
+             <div className="volume-controls">
+               <div className="amplitude-mute amplitude-not-muted"></div>
+               <input type="range" className="amplitude-volume-slider" />
+               <div className="ms-range-fix"></div>
+             </div>
+             <div
+               className="amplitude-shuffle amplitude-shuffle-off"
+               id="shuffle-right"
+             ></div>
+           </div>
          </div>
-          {/* <div id="meta-container">
-            <span data-amplitude-song-info="name" className="song-name"></span>
-            <div className="song-artist-album">
-              <span data-amplitude-song-info="artist"></span>
-              <span data-amplitude-song-info="album"></span>
-            </div>
-          </div> */}
+        <div id="time-container">
+           {/* <span className="current-time">
+             <span className="amplitude-current-minutes"></span>:
+             <span className="amplitude-current-seconds"></span>
+           </span> */}
+           <div id="progress-container">
+             <div className="amplitude-wave-form"></div>
+             <input type="range" className="amplitude-song-slider" />
+             <progress
+               id="song-played-progress"
+               className="amplitude-song-played-progress"
+             ></progress>
+             <progress
+               id="song-buffered-progress"
+               className="amplitude-buffered-progress"
+               value="0"
+             ></progress>
+           </div>
+           {/* <span className="duration">
+             <span className="amplitude-duration-minutes"></span>:
+             <span className="amplitude-duration-seconds"></span>
+           </span> */}
+         </div> 
         </div>
+         {/* <div id="meta-container">
+           <span data-amplitude-song-info="name" className="song-name"></span>
+           <div className="song-artist-album">
+             <span data-amplitude-song-info="artist"></span>
+             <span data-amplitude-song-info="album"></span>
+           </div>
+         </div> */}
+       </div>
+        </div>
+       
       </div>
     </div>
   );
